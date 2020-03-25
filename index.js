5231ecb30eb71de1e9381ad108f5936832675d7d
Michael König <koenigmichael2002@gmail.com>
2020-03-25 11:50:47 +0100

    Refactor code

--- index.js
+++ index.js
@@ -4,21 +4,7 @@ const Telegraf = require("telegraf")
 const bot = new Telegraf(process.env.BOT_TOKEN)
 const schedule = require("node-schedule")
 const { Sequelize, Model, DataTypes } = require("sequelize")
-const { getBundeslandMap, getBundeslandString } = require("./lib.js")
-
-Date.prototype.addHours = function (h) {
-    this.setTime(this.getTime() + h * 60 * 60 * 1000)
-    return this
-}
-
-Date.prototype.addDays = function (d) {
-    this.setTime(this.getTime() + d * 24 * 60 * 60 * 1000)
-    return this
-}
-
-const cloneDate = date => {
-    return new Date(date.getTime())
-}
+const { getBundeslandMap, formatDataToString, DateUtils } = require("./lib.js")
 
 const rule = new schedule.RecurrenceRule()
 rule.minute = [0, 15, 30, 45]
@@ -49,15 +35,10 @@ SimpleData.init(
     { sequelize, modelName: "simple_data", timestamps: false }
 )
 
-const getDateFromString = str => {
-    const [day, month, yearAndTime, _] = str.split(".")
-    const [year, time] = yearAndTime.split(" ")
-    const [hour, minutes] = time.split(":")
-    return new Date(year, month - 1, day, hour, minutes)
-}
+
 
 const insertIntoDB = (data) => {
-    const LetzteAktualisierung = getDateFromString(data.LetzteAktualisierung)
+    const LetzteAktualisierung = DateUtils.getDateFromString(data.LetzteAktualisierung)
     SimpleData.create({
         ...data,
         LetzteAktualisierung,
@@ -67,21 +48,22 @@ const insertIntoDB = (data) => {
 }
 
 const getExtendedData = async data => {
-    const LetzteAktualisierung = getDateFromString(data.LetzteAktualisierung)
+    const LetzteAktualisierung = DateUtils.getDateFromString(data.LetzteAktualisierung)
     const hourEarlier = await SimpleData.findOne({
         where: {
             LetzteAktualisierung:
-                cloneDate(LetzteAktualisierung).addHours(-1)
+                DateUtils.addHours(LetzteAktualisierung, -1)
         }
     })
     const dayEarlier = await SimpleData.findOne({
         where: {
-            LetzteAktualisierung: cloneDate(LetzteAktualisierung).addDays(-1)
+            LetzteAktualisierung: DateUtils.addDays(LetzteAktualisierung, -1)
+
         }
     })
 
-    console.log(cloneDate(LetzteAktualisierung).addHours(-1))
-    console.log(hourEarlier)
+    console.log("earli")
+    console.log(dayEarlier)
 
     let result = { LetzteAktualisierung: data.LetzteAktualisierung }
 
@@ -97,52 +79,7 @@ const getExtendedData = async data => {
     return result
 }
 
-const growthToString = (oldVal, newVal, timePassed) => {
-    let growth = (newVal / oldVal) - 1
-    growth = (growth * 100).toFixed(2)
-    if (growth >= 0) {
-        growth = `+${growth}`
-    }
-    return `${growth}%/${timePassed}`
-}
 
-const formatDataToString = data => {
-    let result = ""
-    for (key of ["Erkrankungen", "Hospitalisiert", "Intensivstation"]) {
-        result += `${key}: ${data[key].current}`
-        if (data[key].hourEarlier != null) {
-            result += ` ${growthToString(data[key].hourEarlier, data[key].current, "1h")}`
-        }
-        if (data[key].dayEarlier != null) {
-            result += ` ${growthToString(data[key].dayEarlier, data[key].current, "1h")}`
-        }
-        result += "\n"
-    }
-
-    const { Erkrankungen, Hospitalisiert, Intensivstation, LetzteAktualisierung, ...dataBundesland } = data
-    let arrBundesland = []
-    for (key in dataBundesland) {
-        arrBundesland.push([key, dataBundesland[key]])
-    }
-
-    arrBundesland.sort((b1, b2) => b2[1].current - b1[1].current)
-
-    for (entry of arrBundesland) {
-        let [key, val] = entry
-        result += `${key}: ${val.current}`
-        if (val.hourEarlier != null) {
-            result += ` [${growthToString(val.hourEarlier, val.current, "1h")}]`
-        }
-        if (val.dayEarlier != null) {
-            result += ` [${growthToString(val.dayEarlier, val.current, "1h")}]`
-        }
-        result += "\n"
-    }
-    result += `\n_Stand: ${data.LetzteAktualisierung}_\n`
-
-
-    return result
-}
 
 const sendUpdate = async () => {
     let res = await axios.get(`${process.env.DATA_URL}/data/SimpleData.js`)
--- lib.js
+++ lib.js
@@ -1,19 +1,94 @@
 const getBundeslandMap = dpBundesland => {
-  let map = {}
-  for (let item of dpBundesland) {
-    map[item.label] = item.y
-  }
-  return map
+    let map = {}
+    for (let item of dpBundesland) {
+        map[item.label] = item.y
+    }
+    return map
 }
 
 const getBundeslandString = dpBundesland => {
-  dpBundesland.sort((b1, b2) => b2.y - b1.y)
+    dpBundesland.sort((b1, b2) => b2.y - b1.y)
 
-  let res = ""
-  for (let item of dpBundesland) {
-    res += `${item.label}: ${item.y}\n`
-  }
-  return res
+    let res = ""
+    for (let item of dpBundesland) {
+        res += `${item.label}: ${item.y}\n`
+    }
+    return res
 }
 
-module.exports = { getBundeslandMap, getBundeslandString }
+const growthToString = (oldVal, newVal, timePassed) => {
+    let growth = (newVal / oldVal) - 1
+    growth = (growth * 100).toFixed(2)
+    if (growth >= 0) {
+        growth = `+${growth}`
+    }
+    return `${growth}%/${timePassed}`
+}
+
+const formatDataToString = data => {
+    let result = ""
+    for (key of ["Erkrankungen", "Hospitalisiert", "Intensivstation"]) {
+        result += `${key}: ${data[key].current}`
+        if (data[key].hourEarlier != null) {
+            result += `, ${growthToString(data[key].hourEarlier, data[key].current, "1h")}`
+        }
+        if (data[key].dayEarlier != null) {
+            result += `, ${growthToString(data[key].dayEarlier, data[key].current, "24h")}`
+        }
+        result += "\n"
+    }
+
+    result += "\n"
+
+    const { Erkrankungen, Hospitalisiert, Intensivstation, LetzteAktualisierung, ...dataBundesland } = data
+    let arrBundesland = []
+    for (key in dataBundesland) {
+        arrBundesland.push([key, dataBundesland[key]])
+    }
+
+    arrBundesland.sort((b1, b2) => b2[1].current - b1[1].current)
+
+    for (entry of arrBundesland) {
+        let [key, val] = entry
+        result += `${key}: ${val.current}`
+        if (val.hourEarlier != null) {
+            result += `, ${growthToString(val.hourEarlier, val.current, "1h")}`
+        }
+        if (val.dayEarlier != null) {
+            result += `, ${growthToString(val.dayEarlier, val.current, "24h")}`
+        }
+        result += "\n"
+    }
+    result += `\n_Stand: ${data.LetzteAktualisierung}_\n`
+
+
+    return result
+}
+
+const DateUtils = {
+    cloneDate: date => {
+        return new Date(date.getTime())
+    },
+    addHours: (date, h) => {
+        newDate = DateUtils.cloneDate(date)
+        newDate.setTime(newDate.getTime() + h * 60 * 60 * 1000)
+        return newDate
+    },
+    addDays: (date, d) => {
+        newDate = DateUtils.cloneDate(date)
+        newDate.setTime(newDate.getTime() + d * 24 * 60 * 60 * 1000)
+        return newDate
+    },
+    getDateFromString: str => {
+        const [day, month, yearAndTime, _] = str.split(".")
+        const [year, time] = yearAndTime.split(" ")
+        const [hour, minutes] = time.split(":")
+        return new Date(year, month - 1, day, hour, minutes)
+    }
+}
+
+
+
+
+
+module.exports = { DateUtils, getBundeslandMap, getBundeslandString, growthToString, formatDataToString }
